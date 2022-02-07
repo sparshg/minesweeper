3bff5aa3a057025ffc570e89bb91ccc3e6e2c4e7
sparshg <43041139+sparshg@users.noreply.github.com>
2022-02-07 20:32:53 +0530

    Layout

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,7 +1,16 @@
 package com.example.tictactoe
 
+import android.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
+import androidx.core.view.setPadding
+import com.airbnb.lottie.LottieAnimationView
 import com.example.tictactoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +22,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.anim.setOnClickListener {
-            binding.anim.playAnimation()
+        for (i in 1..4) {
+            val row = TableRow(this)
+            row.layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT)
+            for (i in 1..4) {
+                val tile = LayoutInflater.from(this).inflate(R.layout.tile, null)
+//                val b = tile.findViewById<TextView>(R.id.tileText)
+//                b.setOnClickListener {
+//                    b.playAnimation()
+//                }
+                row.addView(tile)
+            }
+            binding.tableLayout.addView(row)
         }
     }
 }
